8a98e659d0e4f7574019ea9c5d088e63c7144ded
Jason <byronjason902@gmail.com>
2025-07-25 03:40:27 +0100

    Fix FAQ case sensitivity error

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
+  webpack: (config) => {
+    config.resolve.enforceExtension = false; // Note: it's enforceExtension, not enforceExtensions
+    return config;
+  },
 };
 
 export default nextConfig;
